723f296cdca29829cac4c17e779b17eb4cbb2c77
Ubiratan Lima <ubiratan.lima@gmail.com>
2024-10-19 21:36:36 -0300

    Inicio da V2. Controle de usuarios

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
-
+use App\Http\Controllers\admin\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
 Route::get('/', function () {
     return view('welcome');
