d801eaef36b3e22e66591bd9fb97f08057de2557
mauricio-camargoo <mauriciocamargoo@gmail.com>
2025-05-25 14:54:24 -0300

    Update footer.blade.php
    changetext

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,7 +7,7 @@
                 <a href="{{ route('dashboard') }}">
                     <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                 </a>
-                <span style="padding:0 0 0 10px;">Projeto Integrador 3 - UNIVESP - CAÇAPAVA/CUNHA/JACAREÍ/SÃO JOSÉ DOS CAMPOS -SP</span>
+                <span style="padding:0 0 0 10px;">Projeto Integrador 3 - UNIVESP - CAÇAPAVA/CUNHA/JACAREÍ/SÃO JOSÉ DOS CAMPOS - SP</span>
             </div>
         </div>
     </div>
